92244c6aff164d713fde51804674a21cb9c46295
Myownalx <jummybusinesscentre@gmail.com>
2023-09-08 08:10:11 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - This is a my main put function
+ * Return: Normal 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
