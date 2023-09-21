b7c73306c02f0643a758333efe1bb909528402ab
Myownalx <jummybusinesscentre@gmail.com>
2023-09-21 13:57:07 -0700

    Write a function that encodes a string into 1337

--- 0x06-pointers_arrays_strings/7-main.c
+++ 0x06-pointers_arrays_strings/7-main.c
@@ -10,8 +10,9 @@ int main(void)
 {
     char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
     char *p;
-
+    
     p = leet(s);
+    
     printf("%s", p);
     printf("%s", s);
     return (0);
