706e457943eee60d334c00d28c65fe9fe8c9023d
Myownalx <jummybusinesscentre@gmail.com>
2023-10-01 19:53:05 +0100

    06 last task i am doing

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 int actual_sqrt_recursion(int n, int i);
 /**
  * _sqrt_recursion - returns the natural square root of a number
@@ -11,7 +12,7 @@ int _sqrt_recursion(int n)
 
 	if (n < 0)
 	return (-1);
-	return (actual_sqrt_recursion(n, 0));
+	return (actual_sqrt_recursion (n, 0));
 }
 
 /**
