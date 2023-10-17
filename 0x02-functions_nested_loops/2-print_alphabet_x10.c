4757bd3658eff66c6fb84601c0de3c67d781adb7
Myownalx <jummybusinesscentre@gmail.com>
2023-10-17 19:01:10 +0100

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints the alphabet, in lowercase, ten times followed by a new line
+ * print_alphabet_x10 - prints the alphabet, in lowercase, ten times
+ * followed by a new line
  */
 
 void print_alphabet_x10(void)
@@ -21,7 +22,7 @@ void print_alphabet_x10(void)
                 _putchar(ch);
                 ch++;
         }
-	
+
         _putchar('\n');
 	i++;
 	}
