8333fd62437e1d585a25f3dc83f7729056f4e6de
Myownalx <jummybusinesscentre@gmail.com>
2023-10-25 12:47:36 +0100

    Deleted files

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,13 @@
 #include <unistd.h>
+
 /**
- * main - this prints exactly the input on it
- * Return: always 1.
+ * main - name of the function
+ * Return: 1
  */
+
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
 	return (1);
 }
--- 0x0B-malloc_free/README.md
+++
@@ -1,1 +0,0 @@
-This is the readme for 0x0B-malloc_free
