3d02991f353399fbc6352f49d562a3fd51e27bc3
Myownalx <jummybusinesscentre@gmail.com>
2023-10-26 22:34:09 +0100

    4-pow

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,11 +2,11 @@
 
 /**
  * _pow_recursion - returns the value of x raised to the power of y
- * @x: value to raise
- * @y: power
- *
+ * @x: parameter 1
+ * @y: parameter 2
  * Return: result of the power
  */
+
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
