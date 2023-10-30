51612bec3b3280c4d08a70035e638b72bdcfc5a5
Myownalx <jummybusinesscentre@gmail.com>
2023-10-30 12:55:00 +0100

    last one

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,16 +1,15 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 
 /**
- * main - prints the name of the program
- * @argc: number of arguments
- * @argv: array of arguments
- *
+ * main - a program that prints its name, followed by a new line
+ * @argc: arguement count
+ * @argv: argument value, a strings that come after calling function
  * Return: Always 0 (Success)
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%s\n", *argv);
-
+	if (argc > 0)
+		printf("%s\n", argv[0]);
 	return (0);
 }
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,7 +1,1 @@
-#ifndef FILE_MAIN
-#define FILE_MAIN
-
 int _putchar(char c);
-int _atoi(char *s);
-
-#endif
