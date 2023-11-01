b1fbdcdb4433f258874fc6cd93e348845323cac5
Myownalx <jummybusinesscentre@gmail.com>
2023-11-01 23:05:04 +0100

    args

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -25,4 +25,16 @@ char *argstostr(int ac, char **av)
 	if (str == NULL)
 		return (NULL);
 	for (i = 0; i < ac; i++)
-
+	{
+	for (n = 0; av[i][n]; n++)
+	{
+		str[r] = av[i][n];
+		r++;
+	}
+	if (str[r] == '\0')
+	{
+		str[r++] = '\n';
+	}
+	}
+	return (str);
+}
