125c7f718cc9f839afd877c70870898a6bb7aa26
Myownalx <jummybusinesscentre@gmail.com>
2023-11-04 02:07:36 +0100

    string

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -29,4 +29,18 @@ char *_memset(char *s, char b, unsigned int n)
  * Return: pointer to allocated memory
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
 
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(size * nmemb);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	_memset(ptr, 0, nmemb * size);
+
+	return (ptr);
+}
